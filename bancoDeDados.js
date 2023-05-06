2ab0cfb3b5e160bb9fd971c901fcfcbe462ff733
rafaelacorrea <rafaela.f.c.correa@gmail.com>
2023-05-06 16:54:48 -0300

    fix: :bug: correção na credencial MongoDB

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -5,7 +5,7 @@ async function conectaBancoDeDados() {
     try {
         console.log('Conexão com o banco de dados iniciou')
 
-        await mongoose.connect('process.env.MONGO_URL') //Pegando as credenciais de conexão com o MongoDB através do .env
+        await mongoose.connect(process.env.MONGO_URL) //Pegando as credenciais de conexão com o MongoDB através do .env
 
         console.log('Conexão com o banco de dados feita com sucesso!')
     } catch (erro) {
